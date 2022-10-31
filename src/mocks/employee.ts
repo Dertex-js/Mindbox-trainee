48d28125aff45906104c34eca5872adab6a463e2
Dertex-js <ryoamir60@gmail.com>
2022-10-31 19:50:15 +0500

    ts

---
+++ src/mocks/employee.ts
@@ -0,0 +1,7 @@
+import Employee from "../components/types";
+
+export const employee: Employee[] = [
+  { name: "John S.", salary: 800, increase: false, promotion: true, id: 1 },
+  { name: "Alex M.", salary: 2500, increase: false, promotion: false, id: 2 },
+  { name: "Carl W.", salary: 15000, increase: true, promotion: false, id: 3 },
+]
